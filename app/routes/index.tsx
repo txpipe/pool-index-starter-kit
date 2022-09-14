1ba8b19222dc2940bdb7d96cb0dd37045cfae49c
Alejandro Avagnina <avatxus@gmail.com>
2022-09-14 14:13:53 +0000

    fixed tailwind source location and improved UI

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -21,9 +21,12 @@ type Pool = {
 const PoolCard = (props: { data: Pool }) => {
     return (
         <div className="flex flex-col justify-between p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
-            <h5 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">{props.data.name} - {props.data.ticker}</h5>
-            <p className="mb-3 font-normal text-gray-500 dark:text-gray-400">{props.data.description}</p>
-            <a href={props.data.homepage} target="_blank" rel="noreferrer" className="inline-flex items-center text-blue-600 hover:underline self-end">
+            <div>
+                <h5 className="mb-1 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white text-ellipsis overflow-hidden">{props.data.name}</h5>
+                <h6 className="mb-2 text-lg font-semibold tracking-tight text-gray-500">{props.data.ticker}</h6>
+                <p className="mb-3 text-base font-normal text-gray-500 dark:text-gray-400">{props.data.description}</p>
+            </div>
+            <a href={props.data.homepage} target="_blank" rel="noreferrer" className="inline-flex items-center text-blue-600 hover:underline self-end text-sm -mr-4 -mb-4">
                 Open website
                 <svg className="ml-2 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z"></path><path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z"></path></svg>
             </a>
@@ -63,7 +66,7 @@ const CardsWrapper = (props: { data: Pool[] }) => {
     }, [searchValue, props.data])
     
     return (
-        <div className="p-8 container max-w-6xl overflow-auto">
+        <div className="p-8 h-screen container max-w-6xl m-auto">
             <header className="mb-3 py-6 w-full flex flex-col justify-between">
                 <h3 className="text-3xl dark:text-white text-gray-700 font-extrabold">Pool Index Starter Kit</h3>
             </header>
